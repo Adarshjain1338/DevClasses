4bce8f13bc8e33158ca0e5735877adab29371605
Adarsh jain <adarshjain1338@gmail.com>
2022-01-03 01:37:02 +0530

    2jan Not fully functioned till now with class

--- 29_30 dec 21/main2.js
+++ 29_30 dec 21/main2.js
@@ -1,34 +1,37 @@
 (function () {
     let btn = document.querySelector('#firstbtn');
-    let divcontainer= document.querySelector('#container');
+    let divbreadCrumb = document.querySelector('#divBreadCrumb');
+    let divcontainer = document.querySelector('#container');
     let pageTemplates = document.querySelector('#MyTemplate');
     let folders = [];
-    let fid =-1;
+    let cfid = -1;
+    let fid = -1;
 
     btn.addEventListener("click", addFolder);
     function addFolder() {
         let fname = prompt("Enter a new Folder's Name !");
-        if(!!fname){
-            let fidx = folders.findIndex(f => f.name == fname);
-            if(fidx == -1){
+        if (!!fname) {
+            let exists = folders.some(f => f.name == fname);
+            if (exists == false) {
                 fid++;
                 //RAM
                 folders.push({
                     id: fid,
-                    name: fname
+                    name: fname,
+                    pid: cfid
                 });
+                addHtmlFolder(fname, fid, cfid);
+                saveToStorage();
             }
-            else{
+            else {
                 alert(fname + " Already Exists !")
             }
             //HTML
-            addHtmlFolder(fname,fid);
         }
-        else{
+        else {
             alert("Please Enter Something")
         }
         //STORAGE
-        saveTOStoarge();
     }
 
 
@@ -40,17 +43,17 @@
         let nfname = prompt("Enter new name for " + ofname);
         if (!!nfname) {
             if (nfname != ofname) {
-                let exists = folders.some(f => f.name == nfname);
+                let exists = folders.filter(f => f.pid == cfid).some(f => f.name == nfname);
                 if (exists == false) {
-                   // ram
-                   let folder = folders.find(f => f.name == ofname);
-                   folder.name = nfname;
+                    // ram
+                    let folder = folders.filter(f => f.pid == cfid).find(f => f.name == ofname);
+                    folder.name = nfname;
 
-                   // html
-                   divName.innerHTML = nfname;
+                    // html
+                    divName.innerHTML = nfname;
 
-                   // storage
-                   saveToStorage();
+                    // storage
+                    saveToStorage();
                 } else {
                     alert(nfname + " already exists");
                 }
@@ -62,43 +65,72 @@
         }
     }
 
+    function viewFolder() {
+        let divfolder = this.parentNode;
+        let divName = divfolder.querySelector("[purpose = 'name']");
+        cfid = parseInt(divfolder.getAttribute("fid"));
 
+        let aPathTemplate = pageTemplates.content.querySelector(".path");
+        let aPath = document.importNode(aPathTemplate, true);
 
-    function deleteFolder(){
+        aPath.innerHTML = divName.innerHTML;
+        divbreadCrumb.appendChild(aPath);
+
+        divcontainer.innerHTML = "";
+        folders
+            .filter(f => f.pid == cfid)
+            .forEach(f => { addHtmlFolder(f.name, f.id, f.pid); })
+    }
+
+    function deleteFolder() {
         let divfolder = this.parentNode;
         let divName = divfolder.querySelector("[purpose='name']");
-        let flag =confirm("are you sure you want to delete "+ divName.innerHTML+ " ?");
-        if(flag==true){
+        let fidtbd = divfolder.getAttribute("fid");
+
+        let flag = confirm("are you sure you want to delete " + divName.innerHTML + " ?");
+        if (flag == true) {
             // RAM
-            let fidx = folders.findIndex(f => f.name == divName.innerHTML);
-            folders.splice(fidx ,1);
-            //HTML
-            divcontainer.removeChild(divfolder);
-            //storage
-            saveTOStoarge();
+            let exists = folders.some(f => f.pid == fidtbd);
+            if (exists == false) {
+                let fidx = folders.findIndex(f => f.id == fidtbd);
+                folders.splice(fidx, 1);
+
+                // html
+                divcontainer.removeChild(divfolder);
+
+                // storage
+                saveToStorage();
+            } else {
+                alert("Can't delete. Has children.");
+            }
+
         }
     }
 
 
-    function addHtmlFolder(fname,fid) {
+    function addHtmlFolder(fname, fid, pid) {
         let divFolderTemplate = pageTemplates.content.querySelector(".folder");
-        let divfolder = document.importNode(divFolderTemplate ,true )
+        let divfolder = document.importNode(divFolderTemplate, true)
 
         let divName = divfolder.querySelector("[purpose = 'name']");
-        let spanEdit= divfolder.querySelector("[action = 'edit']");
-        let spanDelete= divfolder.querySelector("[action = 'delete']");
+        let spanEdit = divfolder.querySelector("[action = 'edit']");
+        let spanView = divfolder.querySelector("[action = 'view'");
+        let spanDelete = divfolder.querySelector("[action = 'delete']");
 
-        divName.setAttribute("fid", fid);
+
+        divfolder.setAttribute("fid", fid);
+        divfolder.setAttribute("pid", pid);
         divName.innerHTML = fname;
         spanEdit.addEventListener("click", editFolder);
+        spanView.addEventListener("click", viewFolder)
         spanDelete.addEventListener("click", deleteFolder);
-        
+
 
         divcontainer.appendChild(divfolder);
     }
 
 
-    function saveTOStoarge() {
+    function saveToStorage() {
         let fjson = JSON.stringify(folders);
         localStorage.setItem("data", fjson);
     }
@@ -106,17 +138,19 @@
 
     function loadFromStorage() {
         let fJson = localStorage.getItem("data");
-        if(!!fJson){
+        if (!!fJson) {
             folders = JSON.parse(fJson);
             folders.forEach(f => {
                 if (f.id > fid) {
                     fid = f.id;
                 }
-                addHtmlFolder(f.name, f.id);
+                if (f.pid === cfid) {
+                    addHtmlFolder(f.name, f.id, cfid);
+                }
             });
         }
     }
     loadFromStorage();
-    
+
 
 })();
